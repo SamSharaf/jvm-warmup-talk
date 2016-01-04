d1002244d07da806511174c0e87e01ab8e4f74fb
pricem <pricem@lmax.com>
2016-01-04 10:26:55 +0000

    fixed loop

---
+++ src/main/java/com/epickrram/talk/warmup/example/loop/FixedLoopCount.java
@@ -0,0 +1,15 @@
+package com.epickrram.talk.warmup.example.loop;
+
+public final class FixedLoopCount
+{
+    public static int doLoop()
+    {
+        int sum = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
---
+++ src/main/java/com/epickrram/talk/warmup/example/loop/FixedLoopCountMain.java
@@ -0,0 +1,12 @@
+package com.epickrram.talk.warmup.example.loop;
+
+public final class FixedLoopCountMain
+{
+    public static void main(final String[] args)
+    {
+        while(FixedLoopCount.doLoop() != 0L)
+        {
+            // ignore this checkstyle
+        }
+    }
+}
